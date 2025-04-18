05f50900bfb476524f82cc83de3f67bc1a951c18
TIANDA-ZHANG <tianda.zhang@ibm.com>
2025-04-18 13:53:05 +0900

    update

--- main.tf
+++ main.tf
@@ -11,13 +11,48 @@ provider "tfe" {}
 
 module "random_pets_project" {
   source  = "alexbasista/projecter/tfe"
-  version = "0.1.1"
+  version = "0.1.0"
 
   organization = var.organization
   project_name = "random-pets-aplication"
 
   team_access = {
     terraform-platform-team = "admin"
-    random-pets-app-team = "writer"
+    random-pets-app-team = "write"
   }
+}
+
+module "random_pets_1_mgmt_ws" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.12.0"
+
+  organization   = var.organization
+  workspace_name = "random-pets-1-mgmt-ws"
+  workspace_desc = "Workspace mananging all other Workspaces in this project as code."
+  workspace_tags = ["mgmt"]
+  project_name   = "random-pets-1-application"
+}
+
+module "random_pets_2_project" {
+  source  = "alexbasista/projecter/tfe"
+  version = "0.1.0"
+
+  organization = var.organization
+  project_name = "random-pets-2-application"
+
+  team_access = {
+    terraform-platform-team = "admin"
+    random-pets-app-team    = "write"
+  }
+}
+
+module "random_pets_2_mgmt_ws" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.12.0"
+
+  organization   = var.organization
+  workspace_name = "random-pets-2-mgmt-ws"
+  workspace_desc = "Workspace mananging all other Workspaces in this project as code."
+  workspace_tags = ["mgmt"]
+  project_name   = "random-pets-2-application"
 }
